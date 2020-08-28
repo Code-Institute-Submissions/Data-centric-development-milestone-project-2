93100d875ff0c385389cbdfbd8e0671b2a4095fb
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-08-28 00:00:33 +0000

    connected to the database

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://root1:Ranger12@myfirstcluster.xtvot.mongodb.net/cookbook?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/recipe_index')
+def recipe_index():
+    return render_template("all_recipe.html", recipe=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
