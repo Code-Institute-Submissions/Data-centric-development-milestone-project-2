802054d500cf2181b44a49d7ca374ca94c9c5ef3
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-08-28 12:59:14 +0000

    created add_recipe.html,recipe_list.html,view_recipe.html

--- app.py
+++ app.py
@@ -11,9 +11,18 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/recipe_index')
-def recipe_index():
-    return render_template("all_recipe.html", recipe=mongo.db.recipe.find())
+
+@app.route('/recipe_list')
+def recipe_list():
+    return render_template("recipe_list.html", recipe=mongo.db.recipe.find())
+
+@app.route('/view_recipe')
+def view_recipe():
+    return render_template("view_recipe.html", recipe=mongo.db.recipe.find())   
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html')
 
 
 if __name__ == '__main__':
